25ab30412087d1f295536ae893a73b8e4721d3fc
ohodegaa <ohodegaa@stud.ntnu.no>
2016-10-27 12:34:12 +0200

    Implementert __init__ function

--- Robotic_controller.py
+++ Robotic_controller.py
@@ -3,3 +3,16 @@ __author__ = 'ohodegaa'
 import basic_robot
 
 class BBCON:
+
+
+    def __init__(self, arbitrator):
+        """
+        Initiates the Behavior-Based Robotic Controller
+        :param arbitrator: arbitrator, will provide behaviors?
+        """
+        self.behaviors = []
+        self.active_behaviors = []
+        self.sensobs = []
+        self.motobs = []
+        self.arbitrator = arbitrator
+
