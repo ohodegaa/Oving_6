497bcc984c1999e257b95c453493b077c9a791f4
ohodegaa <ohodegaa@stud.ntnu.no>
2016-10-27 14:31:47 +0200

    Implemented add_behavior, add_sensob, activate_behavior, deactivate_behavior

--- Robotic_controller.py
+++ Robotic_controller.py
@@ -17,7 +17,40 @@ class BBCON:
         self.arbitrator = arbitrator
 
         #self.current_timestep = 0
-        #self.inactive_behaviors = []
+        self.inactive_behaviors = []
         #self.controlled_robot = ""
 
+
+    def add_behavior(self, behavior):
+        """
+        Adds a behavior object to the behaviors list
+        :param behavior: Behavior-object
+        :return:
+        """
+        self.behaviors.append(behavior)
+        self.inactive_behaviors.append(behavior)
+
+    def add_sensob(self, sensob):
+        """
+        Adds a sensob object to the sensob list
+        :param sensob: Sensob object
+        :return:
+        """
+        self.sensobs.append(sensob)
+
+    def activate_behavior(self, behavior):
+        """
+        Activates a behavior
+        :param behavior: Behavior object
+        :return:
+        """
+        self.active_behaviors.append(self.inactive_behaviors.pop(self.inactive_behaviors.index(behavior)))
+
+    def deactivate_behavior(self, behavior):
+        """
+        Deactivates a behavior
+        :param behavior: Behavior object
+        :return:
+        """
+        self.inactive_behaviors.append(self.active_behaviors.pop(self.active_behaviors.index(behavior)))
 
