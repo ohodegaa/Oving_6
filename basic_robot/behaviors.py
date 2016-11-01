791ddcccc9298835931cd24b83ad3bf7f68a58a1
ohodegaa <ohodegaa@stud.ntnu.no>
2016-11-01 10:23:19 +0100

    behaviors

--- basic_robot/behaviors.py
+++ basic_robot/behaviors.py
@@ -1,8 +1,26 @@
 __author__ = 'ohodegaa'
 
+from basic_robot import camera, imager2, irproximity_sensor, reflectance_sensors, ultrasonic
+from robotic_controller import BBCON
+
 
 class Behavior:
-    def __init__(self):
+
+    def __init__(self, bbcon:BBCON, sensobs:list):
+        self.bbcon = bbcon
+        self.sensobs = sensobs
+        self.
+
+    def consider_deactivation(self):
+        pass
+
+    def consider_activation(self):
+        pass
+
+    def update(self):
+        pass
+
+    def sense_and_act(self):
         pass
 
 class Wander(Behavior):
