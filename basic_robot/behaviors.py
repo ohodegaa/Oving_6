a7557d3f7e6012ad0025a4b3e6ee55567434cec0
ohodegaa <ohodegaa@stud.ntnu.no>
2016-11-01 15:24:48 +0100

    laget test modul

--- basic_robot/behaviors.py
+++ basic_robot/behaviors.py
@@ -2,29 +2,38 @@ __author__ = 'ohodegaa'
 
 from basic_robot import camera, imager2, irproximity_sensor, reflectance_sensors, ultrasonic
 from robotic_controller import BBCON
+from abc import ABCMeta, abstractmethod
 
 
-class Behavior:
-
-    def __init__(self, bbcon:BBCON, sensobs:list):
+class Behavior(metaclass=ABCMeta):
+    def __init__(self, bbcon: BBCON, priority):
         self.bbcon = bbcon
-        self.sensobs = sensobs
-        self.
+        self.sensobs = bbcon.sensobs
+        self.active_flag = True
+        self.motor_recomendations = {} #dict[key:motob function, val:function args]
+        self.halt_request = False
+        self.priority = priority
+        self.match_degree = 1
+        self.weigth = self.priority * self.match_degree
 
+    @abstractmethod
     def consider_deactivation(self):
         pass
 
+    @abstractmethod
     def consider_activation(self):
         pass
 
+    @abstractmethod
     def update(self):
-        pass
+        if self.active_flag:
+            self.consider_deactivation()
+        else:
+            self.consider_activation()
 
+    @abstractmethod
     def sense_and_act(self):
         pass
 
-class Wander(Behavior):
-    pass
 
-class FollowRobot(Behavior):
-    pass
+
