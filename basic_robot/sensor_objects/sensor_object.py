879c23cc01213b36f4d8898ed094df1cc6265e0e
ohodegaa <ohodegaa@stud.ntnu.no>
2016-11-04 20:00:15 +0100

    Implemented SensorObject

---
+++ basic_robot/sensor_objects/sensor_object.py
@@ -0,0 +1,23 @@
+__author__ = 'ohodegaa'
+
+from abc import ABCMeta
+
+
+class SensorObject(metaclass=ABCMeta):
+
+    def __init__(self):
+        self.sensors = []
+        self.value = None
+
+    def update(self):
+        for sensor in self.sensors:
+            sensor.update()
+
+    def get_value(self):
+        return self.value
+
+    def reset(self):
+        for sensor in self.sensors:
+            sensor.reset()
+
+
