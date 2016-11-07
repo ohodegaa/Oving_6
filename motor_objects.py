6f56c82f8499ed3fb1a612a5c9b6b67536facb09
ohodegaa <ohodegaa@stud.ntnu.no>
2016-11-07 20:36:43 +0100

    testing FollowLine

--- motor_objects.py
+++ motor_objects.py
@@ -28,10 +28,10 @@ class BeltsController:
         self.motor.set_value([self._default_speed, self._default_speed], dur)
 
     def turn_left(self, degree):
-        self.motor.set_value([self._default_speed * (1 - degree), self._default_speed])
+        self.motor.set_value([0, self._default_speed], dur=0.1)
 
     def turn_right(self, degree):
-        self.motor.set_value([self._default_speed, self._default_speed * (1 - degree)])
+        self.motor.set_value([self._default_speed, 0], dur=0.1)
 
     def random(self, rand_int):
         self.motor.set_value([self._default_speed*rand_int, self._default_speed*rand_int])
