a589b3923fdbe33f4f59a7412a2383a4297a8b6a
ohodegaa <ohodegaa@stud.ntnu.no>
2016-11-08 11:44:33 +0100

    testing FollowLine

--- motor_objects.py
+++ motor_objects.py
@@ -9,7 +9,6 @@ class BeltsController:
     _default_speed = 0.6
 
     def __init__(self):
-        self.motor = Motors()
         self.value = None  # [(function, *args)...]
 
     def update(self, recommendation):
@@ -26,10 +25,10 @@ class BeltsController:
         Motors().set_value([self._default_speed, -self._default_speed], self._sharp_turn_dur)
 
     def backwards(self, dur=None):
-        self.motor.set_value([-self._default_speed, -self._default_speed], dur)
+        Motors().set_value([-self._default_speed, -self._default_speed], dur)
 
     def forward(self, dur=None):
-        self.motor.set_value([self._default_speed, self._default_speed], dur)
+        Motors().set_value([self._default_speed, self._default_speed], dur)
 
     def set_value(self, left_val, right_val):
         print("left: ", left_val)
@@ -38,10 +37,10 @@ class BeltsController:
         Motors().set_value([left_val * self._default_speed, right_val * self._default_speed])
 
     def random(self, rand_int):
-        self.motor.set_value([self._default_speed * rand_int, self._default_speed * rand_int])
+        Motors().set_value([self._default_speed * rand_int, self._default_speed * rand_int])
 
     def stop(self):
-        self.motor.set_value([0, 0])
+        Motors().set_value([0, 0])
 
     def operationalize(self):
         for (func, args) in self.value:
