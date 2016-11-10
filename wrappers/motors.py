6718e72b0ecb69ead01a20eb2950a944a126e9a3
ohodegaa <ohodegaa@stud.ntnu.no>
2016-11-10 22:34:48 +0100

    fixed motors

--- wrappers/motors.py
+++ wrappers/motors.py
@@ -59,14 +59,14 @@ class Motors():
             self.set_right_speed(450)
         self.persist(dur)
 
-    def tilt_left(self, speed=0.8, dur=None):
-        self.set_left_speed(200)
+    def tilt_left(self, speed=0.6, dur=None):
+        self.set_left_speed(150)
         self.set_right_speed(int(self.max*speed))
         self.persist(dur)
 
-    def tilt_right(self, speed=0.8, dur=None):
+    def tilt_right(self, speed=0.6, dur=None):
         self.set_left_speed(int(self.max*speed))
-        self.set_right_speed(200)
+        self.set_right_speed(150)
         self.persist(dur)
 
     def right(self, speed=0.25, dur=None):
