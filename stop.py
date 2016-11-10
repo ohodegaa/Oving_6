4933d6b85a756f8cd09eb443d5f33f89d714d187
ohodegaa <ohodegaa@stud.ntnu.no>
2016-11-10 23:01:20 +0100

    fixed motors

--- stop.py
+++ stop.py
@@ -3,10 +3,8 @@ __author__ = 'ohodegaa'
 from wrappers.motors import Motors
 from zumo_button import ZumoButton
 
-def main():
-    m = Motors()
-    ZumoButton().wait_for_press()
-    m.stop()
 
-if __name__ == '__main__':
-    main()
+m = Motors()
+print("stop")
+ZumoButton().wait_for_press()
+m.stop()
