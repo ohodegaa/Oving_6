9ff733c58f7fdebede4e49eb2f092fbaa0a8a14d
ohodegaa <ohodegaa@stud.ntnu.no>
2016-11-11 13:41:52 +0100

    cleaned up all files

--- robotic_controller.py
+++ robotic_controller.py
@@ -115,11 +115,11 @@ def main():
     ZumoButton().wait_for_press()
     bbcon = BBCON()
     bbcon.add_sensob(FloorSensor())
-    #bbcon.add_sensob(CameraSensor())
+    bbcon.add_sensob(CameraSensor())
     #bbcon.add_sensob(SideSensor())
-    #bbcon.add_sensob(FrontSensor())
+    bbcon.add_sensob(FrontSensor())
     bbcon.add_behavior(FollowLine(bbcon, 0.8))
-    #bbcon.add_behavior(AvoidObject(bbcon ,1.0))
+    bbcon.add_behavior(AvoidObject(bbcon ,1.0))
     #bbcon.add_behavior(SideSight(bbcon, 0.7))
 
 
