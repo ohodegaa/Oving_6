0b802818ea120b5530c7777ebfd7f3d6cc169bac
ohodegaa <ohodegaa@stud.ntnu.no>
2016-11-14 09:26:06 +0100

    cleaned up all files

--- robotic_controller.py
+++ robotic_controller.py
@@ -107,7 +107,7 @@ class BBCON:
         self.update_all_sensobs()
         self.update_all_behaviors()
         self.choose_action()
-        #self.wait(0.12345)
+        self.wait(0.085)
         self.reset_sensobs()
 
 
